0f83bfcdbe8d38cebdb164b57647a10058ec1c22
OganesK <53556592+OganesK@users.noreply.github.com>
2021-12-04 00:44:34 +0300

    Update lab3.java

--- src/lab3.java
+++ src/lab3.java
@@ -1,73 +1,75 @@
 public class lab3 {
-  private static int[] origin;
-  private static int length;
-  private int lt;
-  private int gt;
+    private static int[] origin;
+    private static int length;
+    private int lt;
+    private int gt;
 
-  public static void QuickSort3Way(int len) {
-    length = len;
-    origin = new int[]{0,7,8,1,8,9,3,8,8,8,0,7,8,1,8,9,3,8,8,8};
-  }
+    public  lab3() {//Конструктор
+        origin = new int[]{5,4,3,2,1};
+        length = origin.length;
+    }
 
-  public void swap(int a, int b) {
-    int temp = origin[a];
-    origin[a] = origin[b];
-    origin[b] = temp;
-  }
+    public void swap(int a, int b) {
+        int temp = origin[a];
+        origin[a] = origin[b];
+        origin[b] = temp;
+    }
 
-  public int randomPivot(int start, int end){
-    return (start + (int)(Math.random()*(end-start+1)));
-  }
+    public int randomPivot(int start, int end){
+        return (start + (int)(Math.random()*(end-start+1)));
+    }
 
-  public void partition(int pivot, int start, int end){
-    swap(pivot,start);  // swapping pivot and starting element in that subarray
+    public void partition(int pivot, int start, int end){
+        swap(pivot,start);
 
-    int pivot_value = origin[start];
-    lt = start;
-    gt = end;
+        int pivot_value = origin[start];
+        lt = start;
+        gt = end;
 
-    int i = start;
-    while(i <= gt) {
+        int i = start;
+        while(i <= gt) {
 
-        if(origin[i] < pivot_value) {
-            swap(lt, i);
-            lt++;
-            i++;
+            if(origin[i] < pivot_value) {
+                swap(lt, i);
+                lt++;
+                i++;
+            }
+
+            if(origin[i] > pivot_value) {
+                swap(gt, i);
+                gt--;
+            }
+            if(origin[i] == pivot_value)
+                i++;
         }
+    }
+    public void Sort(int start, int end){
+        if(start < end) {
 
-        if(origin[i] > pivot_value) {
-            swap(gt, i);
-            gt--;
+            int pivot = randomPivot(start,end);
+            partition(pivot, start, end);
+            Sort(start, lt-1);
+            Sort(gt+1, end);
+
         }
-        if(origin[i] == pivot_value)
-            i++;
     }
-}
-public void Sort(int start, int end){
-    if(start < end) {
 
-        int pivot = randomPivot(start,end); // choose the index for pivot randomly
-        partition(pivot, start, end); // about index the array is partitioned
-
-        Sort(start, lt-1);
-        Sort(gt+1, end);
-
+    public void Sort(){
+        Sort(0,length-1);
     }
-}
 
-public void Sort(){
-    Sort(0,length-1);
-}
-
-public void disp(){
-    for(int i=0; i<length;++i){
-        System.out.print(origin[i]+" ");
+    public void disp(){
+        for(int i=0; i<length;++i){
+            System.out.print(origin[i]+" ");
+        }
+        System.out.println();
     }
-    System.out.println();
-}
 
-public static void main(String[] args) {
-
-}
+    public static void main(String[] args) {
+        lab3 lab3 = new lab3();
+        lab3.disp();
+        lab3.Sort();
+        lab3.disp();
+    }
 
 }
